faa770f2c14b6ce70b7960ae7124c6cfbd64e867
gauravparab.2008@rediffmail.com <gauravparab.2008@rediffmail.com>
2020-08-17 23:13:22 +0530

    Initial commit

---
+++ src/main/java/com/springbootdev/reactspringbootcrudapp/ReactSpringBootCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdev.reactspringbootcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdev/reactspringbootcrudapp/ReactSpringBootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdev.reactspringbootcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
